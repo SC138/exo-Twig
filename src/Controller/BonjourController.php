865892148c809166c30dc9c8999c109615410983
Thomas Chevron <chevron.thomas33@gmail.com>
2022-07-01 17:31:17 +0200

    Suite exo PATH twig

--- src/Controller/BonjourController.php
+++ src/Controller/BonjourController.php
@@ -8,24 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class BonjourController extends AbstractController
 {
     /**
-     * @Route("home", name="bonjour")
+     * @Route("/", name="home")
      */
 
 
     public function home () {
 
-        $category = [
-            'title' => 'Politique',
-            'color' => 'red',
-            'published' => true,
-            'description' => "Decrypter l'actualité politique avec le prisme de David Robert"
-        ];
 
-        $name = 'Tom';
 
         return $this->render ('home.html.twig', [
-            'name' => $name,
-            'category' => $category
+
         ]);
 
 
